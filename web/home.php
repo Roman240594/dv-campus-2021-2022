fecb779d00a7173ad1f8b6e78151496266ecb4f0
{r-linevych} <{linevychroman@gmail.com}>
2022-01-31 21:16:58 +0200

    #2 Shop Skeleton: added basic pages: home

---
+++ web/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello, my car!</title>
+</head>
+<body>
+<p style="font-size: 48px;">Hello, my car!</p>
+<img src="1400x936.jpg">
+</body>
+</html>
